8b6229fd70409656257a6446a254fd62fde8a03a
Shivam Mathur <shivam_jpr@hotmail.com>
2026-06-26 03:15:15 +0530

    Apply Winlibs tidy 5.8.0 build patch

--- include/tidyplatform.h
+++ include/tidyplatform.h
@@ -485,7 +485,9 @@ extern "C" {
 #    define utimbuf _utimbuf /* Windows seems to want utimbuf */
 #    define stat _stat
 #    define utime _utime
+#    ifndef vsnprintf
 #    define vsnprintf _vsnprintf
+#    endif
 #  endif
 
 #endif
@@ -515,7 +517,9 @@ extern "C" {
 #endif
 
 #  define access _access
+#  ifndef strcasecmp
 #  define strcasecmp _stricmp
+#  endif
 
 #  ifndef va_copy
 #    define va_copy(dest, src) (dest = src)
